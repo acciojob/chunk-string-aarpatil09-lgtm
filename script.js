5e13d20afb013e63065ce86961ace1000f9653a6
aarpatil09-lgtm <aarpatil09@gmail.com>
2026-04-28 15:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function stringChop(str, size) {
   // your code here
+	let result = [];
+	for(let i=0;i<str.length;i=i+Number(size)){
+		result.push(str.slice(i,i+Number(size)));
+	}
+	return result;
 }
 
 // Do not change the code below
